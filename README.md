0403d5d2590a0e39b5082ab9cb44fcc724b972c8
Marco Michelino <marco.michelino@immobiliare.it>
2024-05-13 21:16:36 +0200

    documentation + improved remove_stake algorithm

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
-# FOMO Staking contract
- 
-Below are the transaction manifests needed to use the contract:
+# FOMO Staking blueprint
 
-## instantiate (stokenet)
+## This blueprint has the following characteristics
+
+* It allows the owner to deposit future rewards ahead of time (just like the HIT rug-proof feature)
+* It allows third parties to airdrop any fungible coin to all FOMO stakers
+* It allows whoever instantiates the component to specify a minimum time that must elapse between stake and unstake
+* Staking receipts are non fungible
+* At each reward distribution an AirdropEvent is issued
+* The add\_stake method is compatible with SelfiSocial guidelines
+
+## Known limitations
+
+When a user unstakes (remove\_stake) he can't receive more than MAX\_BUCKETS (100) different coins because of limitations on the number of events in a Radix transaction.  
+
+## Below are the transaction manifests needed to use this contract:
+
+### Instantiate (Stokenet)
 ```
 CALL_FUNCTION
-  Address("package_tdx_2_1p4xgnn4dad69nq6ls9a5gxm8dt7j29g8vfptwg95mmev5xeyn57g0h")
+  Address("")
   "FomoStaking"
   "new"
   Address("<OWNER_BADGE>")
@@ -14,7 +27,7 @@ CALL_FUNCTION
 ;
 ```
 
-## add stake
+### Add stake
 ```
 CALL_METHOD
   Address("<ACCOUNT>")
@@ -22,26 +35,23 @@ CALL_METHOD
   Address("<FOMO_RESOURCE_ADDRESS>")
   Decimal("<AMOUNT_TO_STAKE>")
 ;
-
 TAKE_ALL_FROM_WORKTOP
   Address("<FOMO_RESOURCE_ADDRESS>")
   Bucket("tokens")
 ;
-
 CALL_METHOD
 	Address("<FOMO_STAKING_COMPONENT_ADDRESS>")
 	"add_stake"
 	Bucket("tokens")
 ;
-
 CALL_METHOD
-    Address("<ACCOUNT>")
-    "deposit_batch"
-    Expression("ENTIRE_WORKTOP")
+  Address("<ACCOUNT>")
+  "deposit_batch"
+  Expression("ENTIRE_WORKTOP")
 ;
 ```
 
-## remove stake
+### Remove stake
 ```
 CALL_METHOD
   Address("<ACCOUNT>>")
@@ -49,26 +59,23 @@ CALL_METHOD
   Address("<STAKED_FOMO_RESOURCE_ADDRESS>")
   Array<NonFungibleLocalId>(NonFungibleLocalId("#<STAKED_FOMO_ID>#"))
 ;
-
 TAKE_ALL_FROM_WORKTOP
   Address("<STAKED_FOMO_RESOURCE_ADDRESS>")
   Bucket("tokens")
 ;
-
 CALL_METHOD
 	Address("<FOMO_STAKING_COMPONENT_ADDRESS>")
 	"remove_stake"
 	Bucket("tokens")
 ;
-
 CALL_METHOD
-    Address("<ACCOUNT>")
-    "deposit_batch"
-    Expression("ENTIRE_WORKTOP")
+  Address("<ACCOUNT>")
+  "deposit_batch"
+  Expression("ENTIRE_WORKTOP")
 ;
 ```
 
-## airdrop
+### Airdrop any fungible
 ```
 CALL_METHOD
   Address("<ACCOUNT>")
@@ -76,12 +83,10 @@ CALL_METHOD
   Address("<RESOURCE_ADDRESS_TO_AIRDROP>")
   Decimal("<AMOUNT_TO_AIRDROP>")
 ;
-
 TAKE_ALL_FROM_WORKTOP
   Address("<RESOURCE_ADDRESS_TO_AIRDROP>")
   Bucket("tokens")
 ;
-
 CALL_METHOD
 	Address("<FOMO_STAKING_COMPONENT_ADDRESS>")
 	"airdrop"
@@ -89,35 +94,7 @@ CALL_METHOD
 ;
 ```
 
-## deposit future rewards
-```
-CALL_METHOD
-  Address("<ACCOUNT>")
-    "create_proof_of_amount"
-    Address("<OWNER_BADGE>")
-    Decimal("1")
-;
-
-CALL_METHOD
-  Address("<ACCOUNT>")
-  "withdraw"
-  Address("<FOMO_RESOURCE_ADDRESS>")
-  Decimal("<AMOUNT>")
-;
-
-TAKE_ALL_FROM_WORKTOP
-  Address("<FOMO_RESOURCE_ADDRESS>")
-  Bucket("tokens")
-;
-
-CALL_METHOD
-  Address("<FOMO_STAKING_COMPONENT_ADDRESS>")
-  "deposit_rewards"
-  Bucket("tokens")
-;
-```
-
-## airdrop part of the previosly deposited rewards
+### Deposit future rewards (owner only)
 ```
 CALL_METHOD
   Address("<ACCOUNT>")
@@ -125,7 +102,31 @@ CALL_METHOD
   Address("<OWNER_BADGE>")
   Decimal("1")
 ;
+CALL_METHOD
+  Address("<ACCOUNT>")
+  "withdraw"
+  Address("<FOMO_RESOURCE_ADDRESS>")
+  Decimal("<AMOUNT>")
+;
+TAKE_ALL_FROM_WORKTOP
+  Address("<FOMO_RESOURCE_ADDRESS>")
+  Bucket("tokens")
+;
+CALL_METHOD
+  Address("<FOMO_STAKING_COMPONENT_ADDRESS>")
+  "deposit_rewards"
+  Bucket("tokens")
+;
+```
 
+### Distribute part of the previosly deposited rewards (owner only)
+```
+CALL_METHOD
+  Address("<ACCOUNT>")
+  "create_proof_of_amount"
+  Address("<OWNER_BADGE>")
+  Decimal("1")
+;
 CALL_METHOD
   Address("<FOMO_STAKING_COMPONENT_ADDRESS>")
   "airdrop_deposited_amount"
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,45 @@
 use scrypto::prelude::*;
 
+// Event to emit at each airdrop.
 #[derive(ScryptoSbor, ScryptoEvent)]
 struct AirdropEvent {
     coin: ResourceAddress,
     amount: Decimal,
 }
 
+// NonFungibleData for the staking receipt
 #[derive(Debug, ScryptoSbor, NonFungibleData)]
 struct StakedFomoData {
+    // When stake happened
     stake_date: Instant,
+
+    // Can not be unstaked before this date
     minimum_unstake_date: Instant,
+
+    // How many FOMO have been staked
     amount_staked: Decimal,
+
+    // A number user to split rewards among the stakers
     stake_share: PreciseDecimal,
+
+    // This receipt only allows receiving rewards for airdrops happening after this one
     last_airdrop_id: u64,
 }
 
+// A struct to store coins and information about non FOMO airdrops happened
 #[derive(Debug, ScryptoSbor)]
 struct Airdrop {
-    vault: Vault,
+    coin: ResourceAddress,
     amount_per_share: PreciseDecimal,
 }
 
+// Metadata for the staking receipt
 static STAKED_FOMO_NAME: &str = "Staked FOMO";
 static STAKED_FOMO_ICON: &str = "https://pbs.twimg.com/media/GEfnpcUbIAAoEY8?format=jpg&name=small";
-static MAX_AIRDROPS_PER_USER: u64 = 100;
+
+// Maximum number of buckets the remove_stake method will return.
+// Raising this limit may cause transactions to fail
+static MAX_BUCKETS: usize = 100;
 
 #[blueprint]
 #[events(AirdropEvent)]
@@ -31,33 +47,68 @@ mod fomo_staking {
 
     enable_method_auth! {
         methods {
+            // Method to stake a bucket of FOMO, it returns a staking receipt
             add_stake => PUBLIC;
+
             remove_stake => PUBLIC;
+
+            // Method to airdrop any fungible coin to current FOMO stakers
             airdrop => PUBLIC;
+
+            // This method allows the owner to deposit future staking rewards in the component ahead of time
             deposit_rewards => restrict_to: [OWNER];
+
+            // This method allows the owner to distribute the previously deposited rewards
             airdrop_deposited_amount => restrict_to: [OWNER];
         }
     }
 
     struct FomoStaking {
+        // Vault to keep the staked FOMO
         fomo_vault: Vault,
+
+        // KVS where the non FOMO coins are stored
+        vaults: KeyValueStore<ResourceAddress, Vault>,
+
+        // KVS where all non FOMO Airdrop objects are stored
         airdrops: KeyValueStore<u64, Airdrop>,
+
+        // Optionally, stake can have a minimum length
         minimum_stake_period: i64,
+
+        // ResourceManager to mint staking receipts
         staked_fomo_resource_manager: ResourceManager,
+
+        // NonFungibleLocalId of the next staking receipt
         next_staked_fomo_id: u64,
+
+        // The sum of all of the stake_share in the staking receipt
         total_stake_share: PreciseDecimal,
+
+        // Optionally, the Owner can deposit the future FOMO rewards in this Vault ahead of time.
         future_rewards: Vault,
+
+        // Numeric index of the last Airdrop object in the airdrops KVS
         last_airdrop_id: u64,
     }
 
     impl FomoStaking {
 
+        // IFunction to instantiate a new FomoStaking component
         pub fn new(
-            owner_badge_address: ResourceAddress,
-            fomo_address: ResourceAddress,
-            minimum_stake_period: i64,
-        ) -> Global<FomoStaking> {
 
+            // The resource to set as owner badge for the component and the staking receipts it will mint
+            owner_badge_address: ResourceAddress,
+
+            // The resource address of FOMO
+            fomo_address: ResourceAddress,
+
+            // The minimum time has to pass from stake to unstake (seconds).
+            // It can be zero to let the user unstake whenever he wants.
+            minimum_stake_period: i64,
+
+        ) -> Global<FomoStaking> {
+            // Make sure minimum_stake_period isn't a negative number
             assert!(
                 minimum_stake_period >= 0,
                 "Invalid minimum_stake_period",
@@ -67,7 +118,7 @@ mod fomo_staking {
             let (address_reservation, component_address) =
                 Runtime::allocate_component_address(FomoStaking::blueprint_id());
 
-            // Create a ResourceManager for minting transient NFTs used by flash loans
+            // Create a ResourceManager for minting staking receipts
             let staked_fomo_resource_manager = ResourceBuilder::new_integer_non_fungible::<StakedFomoData>(
                 OwnerRole::Updatable(rule!(require(owner_badge_address)))
             )
@@ -88,7 +139,7 @@ mod fomo_staking {
                 minter_updater => rule!(deny_all);
             ))
             .non_fungible_data_update_roles(non_fungible_data_update_roles!(
-                non_fungible_data_updater => rule!(deny_all);
+                non_fungible_data_updater => rule!(require(global_caller(component_address)));
                 non_fungible_data_updater_updater => rule!(require(owner_badge_address));
             ))
             .burn_roles(burn_roles!(
@@ -97,8 +148,10 @@ mod fomo_staking {
             ))
             .create_with_no_initial_supply();
 
+            // Instantiate the component and globalize it
             Self {
                 fomo_vault: Vault::new(fomo_address),
+                vaults: KeyValueStore::new(),
                 airdrops: KeyValueStore::new(),
                 minimum_stake_period: minimum_stake_period,
                 staked_fomo_resource_manager: staked_fomo_resource_manager,
@@ -113,29 +166,39 @@ mod fomo_staking {
             .globalize()
         }
 
+        // Method to stake a bucket of FOMO, it returns a staking receipt
         pub fn add_stake(&mut self, fomo: Bucket) -> Bucket {
+            // Make sure the bucket contains FOMO
             assert!(
                 fomo.resource_address() == self.fomo_vault.resource_address(),
                 "Wrong coin bro",
             );
 
+            // Count the coins in the bucket
             let amount = fomo.amount();
             assert!(
                 amount > Decimal::ZERO,
                 "No coin bro",
             );
 
+            // Count the already staked coins
             let vault_amount = self.fomo_vault.amount();
+
+            // Empty the bucket in the vault
             self.fomo_vault.put(fomo);
 
+            // Set stake_share and increase total_stake_share so that stake_share/total_stake_share represents the
+            // share of FOMO in the vault for this user.
             let mut stake_share = PreciseDecimal::ONE;
             if vault_amount > Decimal::ZERO {
                 stake_share = (self.total_stake_share * amount) / vault_amount;
             }
             self.total_stake_share += stake_share;
 
+            // Get current Instant
             let now = Clock::current_time_rounded_to_seconds();
 
+            // Mint a staking receipt
             let staked_fomo = self.staked_fomo_resource_manager.mint_non_fungible(
                 &NonFungibleLocalId::integer(self.next_staked_fomo_id.into()),
                 StakedFomoData {
@@ -148,31 +211,42 @@ mod fomo_staking {
                     last_airdrop_id: self.last_airdrop_id,
                 }
             );
+
+            // Prepare for minting the next staking receipt
             self.next_staked_fomo_id += 1;
 
+            // Return the staking receipt to the user
             staked_fomo
         }
 
         pub fn remove_stake(&mut self, staked_fomo: Bucket) -> Vec<Bucket> {
+            // Make sure staked_fomo is a staking receipt
             assert!(
                 staked_fomo.resource_address() == self.staked_fomo_resource_manager.address(),
                 "Wrong coin bro",
             );
 
-            let now = Clock::current_time_rounded_to_seconds().seconds_since_unix_epoch;
-            
+            // Read NonFungibleData from the bucket (it must contain exactly 1 staking receipt)
             let staked_fomo_data = staked_fomo.as_non_fungible().non_fungible::<StakedFomoData>().data();
+
+            // Make sure the minimum stakeing time has passed
+            let now = Clock::current_time_rounded_to_seconds().seconds_since_unix_epoch;
             assert!(
                 now >= staked_fomo_data.minimum_unstake_date.seconds_since_unix_epoch,
                 "Can't unstake now bro",
             );
 
+            // Burn the staking receipt
             staked_fomo.burn();
 
+            // Compute the % of total FOMO to be unstaked and update total_stake_share accordingly
             let ratio = staked_fomo_data.stake_share / self.total_stake_share;
             self.total_stake_share -= staked_fomo_data.stake_share;
 
-            let mut coins = vec![];
+            // Prepare a vector for all of the buckets to return
+            let mut coins: Vec<Bucket> = vec![];
+
+            // Create the FOMO bucket and add it to the vector
             let mut amount = ratio * PreciseDecimal::from(self.fomo_vault.amount());
             coins.push(
                 self.fomo_vault.take_advanced(
@@ -181,78 +255,118 @@ mod fomo_staking {
                 )
             );
 
-            let first_airdrop_id = staked_fomo_data.last_airdrop_id + 1;
-            let last_airdrop_id = match self.last_airdrop_id < first_airdrop_id + MAX_AIRDROPS_PER_USER {
-                true => self.last_airdrop_id,
-                false => first_airdrop_id + MAX_AIRDROPS_PER_USER - 1,
-            };
-            for airdrop_id in first_airdrop_id..=last_airdrop_id {
-                let mut airdrop = self.airdrops.get_mut(&airdrop_id).unwrap();
+            // Prepare a HashMap to store the total the user must receive per each non FOMO coin
+            let mut totals: HashMap<ResourceAddress, PreciseDecimal> = HashMap::with_capacity(MAX_BUCKETS);
 
+            // For each non FOMO airdrop happened during the staking period
+            for airdrop_id in staked_fomo_data.last_airdrop_id + 1 ..= self.last_airdrop_id {
+                // Find the airdrop information
+                let airdrop = self.airdrops.get_mut(&airdrop_id).unwrap();
+
+                // Compute the amount this user must receive
                 amount = staked_fomo_data.stake_share * airdrop.amount_per_share;
 
+                // Is this coin already in the totals HashMap?
+                if totals.get(&airdrop.coin).is_some() {
+                    // If so, put the coins in this bucket
+                    *totals.get_mut(&airdrop.coin).unwrap() += amount;
+                } else {
+                    // If not, can we create another one without exceeding MAX_BUCKETS?
+                    if totals.len() < MAX_BUCKETS {
+                        // If so, add a new element to totals
+                        totals.insert(airdrop.coin, amount);
+                    } // Otherwise those coins are lost forever :-(
+                }
+            }
+
+            // For each coin the user has to receive
+            for (resource_address, amount) in totals.iter() {
+                // Create a new bucket with the number of coins he has to receve and add it to the coins vector
                 coins.push(
-                    airdrop.vault.take_advanced(
+                    self.vaults.get_mut(&resource_address).unwrap().take_advanced(
                         amount.checked_truncate(RoundingMode::ToZero).unwrap(),
                         WithdrawStrategy::Rounded(RoundingMode::ToZero),
                     )
                 );
             }
 
+            // Give the user all of his coins
             coins
         }
 
+        // Method to airdrop any fungible coin to current FOMO stakers
         pub fn airdrop(&mut self, coins: Bucket) {
+            // Get the resource_address of the coin to airdrop and make sure it is a fungible
             let resource_address = coins.resource_address();
             assert!(
                 resource_address.is_fungible(),
                 "Can't airdrop NFTs",
             );
 
+            // Make sure there are actually coins in the bucket
             let amount = coins.amount();
             assert!(
                 amount > Decimal::ZERO,
                 "No coin bro",
             );
 
+            // If no one is staking, the coins will be lost forever
             assert!(
                 self.staked_fomo_resource_manager.total_supply().unwrap() > Decimal::ZERO,
                 "No one to airdrop to",
             );
 
+            // If the bucket contains some FOMO, just put them in the vault of the staked FOMO
             if resource_address == self.fomo_vault.resource_address() {
                 self.fomo_vault.put(coins);
             } else {
+                // If the bucket contains any fungible different from FOMO, create a new Airdrop object and add it to
+                // the KVS
                 self.last_airdrop_id += 1;
                 self.airdrops.insert(
                     self.last_airdrop_id,
                     Airdrop {
-                        vault: Vault::with_bucket(coins),
+                        coin: resource_address,
                         amount_per_share: amount / self.total_stake_share,
                     }
                 );
+
+                // Does a vault for this coins already exist?
+                if self.vaults.get(&resource_address).is_some() {
+                    // If so, put the coins in it
+                    self.vaults.get_mut(&resource_address).unwrap().put(coins);
+                } else {
+                    // Else create the vault with the coins in it
+                    self.vaults.insert(resource_address, Vault::with_bucket(coins));
+                }
             }
 
+            // Tell everyone the party is here
             Runtime::emit_event(AirdropEvent {
                 coin: resource_address,
                 amount: amount,
             });
         }
 
+        // This method allows the owner to deposit future staking rewards in the component ahead of time
         pub fn deposit_rewards(&mut self, fomo: Bucket) {
             self.future_rewards.put(fomo);
         }
 
+        // This method allows the owner to distribute the previously deposited rewards
         pub fn airdrop_deposited_amount(&mut self, amount: Decimal) {
+            // If no one is staking, the coins will be lost forever
             assert!(
                 self.staked_fomo_resource_manager.total_supply().unwrap() > Decimal::ZERO,
                 "No one to airdrop to",
             );
 
+            // Take the required amount from the future rewards and put it in the vault of the staked FOMO
             self.fomo_vault.put(
                 self.future_rewards.take(amount)
             );
 
+            // Tell everyone the party is here
             Runtime::emit_event(AirdropEvent {
                 coin: self.fomo_vault.resource_address(),
                 amount: amount,
